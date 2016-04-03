4fe7db8ecde3444645c042a9310432fe612b2b86
Benjamin Mohan <benmohan77@gmail.com>
2016-04-03 14:22:23 -0500

    Making sign up screen functional and linking it to main menu

---
+++ src/main/java/com/mohan/ben/kingdoms/Menu.java
@@ -0,0 +1,26 @@
+package com.mohan.ben.kingdoms;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Bundle bundle = getIntent().getExtras();
+        String vEmail = bundle.getString("email");
+
+        setSupportActionBar(toolbar);
+
+
+
+    }
+
+}
